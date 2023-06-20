ca135ea3a9feff32bf216f989c815fb908a16276
Chris Saunders <csaunders@pacificbiosciences.com>
2023-06-19 23:23:44 -0700

    Allow zero gap open cost in affine2p

--- wavefront/wavefront_penalties.c
+++ wavefront/wavefront_penalties.c
@@ -133,11 +133,11 @@ void wavefront_penalties_set_affine2p(
     fprintf(stderr,"[WFA::Penalties] Match score must be negative or zero (M=%d)\n",affine2p_penalties->match);
     exit(1);
   } else if (affine2p_penalties->mismatch <= 0 ||
-             affine2p_penalties->gap_opening1 <= 0 ||
+             affine2p_penalties->gap_opening1 < 0 ||
              affine2p_penalties->gap_extension1 <= 0 ||
-             affine2p_penalties->gap_opening2 <= 0 ||
+             affine2p_penalties->gap_opening2 < 0 ||
              affine2p_penalties->gap_extension2 <= 0) {
-    fprintf(stderr,"[WFA::Penalties] Penalties (X=%d,O1=%d,E1=%d,O2=%d,E2=%d) must be (X>0,O1>=0,E1>0,O1>=0,E1>0)\n",
+    fprintf(stderr,"[WFA::Penalties] Penalties (X=%d,O1=%d,E1=%d,O2=%d,E2=%d) must be (X>0,O1>=0,E1>0,O2>=0,E2>0)\n",
         affine2p_penalties->mismatch,
         affine2p_penalties->gap_opening1,
         affine2p_penalties->gap_extension1,
