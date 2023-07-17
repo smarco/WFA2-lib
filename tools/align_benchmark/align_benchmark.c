6dd429810a2d2c57443a8bb105f7d270d1fb5a07
smarco <santiagomsola@gmail.com>
2023-07-17 19:51:02 +0200

    Changed align_benchmark general global-timer to measure benchmark call (without I/O) to be more informative. Alignment-timer remains the same.

--- tools/align_benchmark/align_benchmark.c
+++ tools/align_benchmark/align_benchmark.c
@@ -442,7 +442,6 @@ void align_benchmark_run_algorithm(
 void align_benchmark_sequential() {
   // PROFILE
   timer_reset(&parameters.timer_global);
-  timer_start(&parameters.timer_global);
   // I/O files
   parameters.input_file = fopen(parameters.input_filename, "r");
   if (parameters.input_file == NULL) {
@@ -465,7 +464,9 @@ void align_benchmark_sequential() {
         seqs_processed,&align_input);
     if (!input_read) break;
     // Execute the selected algorithm
+    timer_start(&parameters.timer_global); // PROFILE
     align_benchmark_run_algorithm(&align_input);
+    timer_stop(&parameters.timer_global); // PROFILE
     // Update progress
     ++seqs_processed;
     if (++progress == parameters.progress) {
@@ -482,7 +483,6 @@ void align_benchmark_sequential() {
     if (parameters.plot != 0) align_benchmark_plot_wf(&align_input,seqs_processed);
   }
   // Print benchmark results
-  timer_stop(&parameters.timer_global);
   if (parameters.verbose >= 0) align_benchmark_print_results(&align_input,seqs_processed,true);
   // Free
   align_benchmark_free(&align_input);
@@ -494,7 +494,6 @@ void align_benchmark_sequential() {
 void align_benchmark_parallel() {
   // PROFILE
   timer_reset(&parameters.timer_global);
-  timer_start(&parameters.timer_global);
   // Open input file
   parameters.input_file = fopen(parameters.input_filename, "r");
   if (parameters.input_file == NULL) {
@@ -528,6 +527,7 @@ void align_benchmark_parallel() {
     }
     if (seqs_batch == 0) break;
     // Parallel processing of the sequences batch
+    timer_start(&parameters.timer_global); // PROFILE
     #pragma omp parallel num_threads(parameters.num_threads)
     {
       int tid = omp_get_thread_num();
@@ -544,6 +544,7 @@ void align_benchmark_parallel() {
         align_benchmark_run_algorithm(align_input+tid);
       }
     }
+    timer_stop(&parameters.timer_global); // PROFILE
     // Update progress
     seqs_processed += seqs_batch;
     progress += seqs_batch;
@@ -555,7 +556,6 @@ void align_benchmark_parallel() {
     //mm_allocator_print(stderr,align_input->wf_aligner->mm_allocator,false);
   }
   // Print benchmark results
-  timer_stop(&parameters.timer_global);
   if (parameters.verbose >= 0) align_benchmark_print_results(align_input,seqs_processed,true);
   // Free
   for (int tid=0;tid<parameters.num_threads;++tid) {
