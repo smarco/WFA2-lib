4d0b9238857554b958ffa20048b155d29be92e04
rlorigro <rlorigro@ucsc.edu>
2023-12-14 13:41:39 -0500

    Switch to types.h

--- wavefront/wavefront_extend_kernels.c
+++ wavefront/wavefront_extend_kernels.c
@@ -29,11 +29,13 @@
  * DESCRIPTION: WFA module for the "extension" of exact matches
  */
 
-#if defined(__APPLE__) || defined(__FreeBSD__)
-#include <machine/endian.h>  // __BYTE_ORDER
-#else
-#include <endian.h>  // __BYTE_ORDER
-#endif
+//#if defined(__APPLE__) || defined(__FreeBSD__)
+//#include <machine/endian.h>  // __BYTE_ORDER
+//#else
+//#include <endian.h>  // __BYTE_ORDER
+//#endif
+
+#include <sys/types.h>
 
 #include "wavefront_extend_kernels.h"
 #include "wavefront_termination.h"
